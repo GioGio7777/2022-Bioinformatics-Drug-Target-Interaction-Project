1f84eac2145dbb55831904934d09ab527a2d13c4
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2022-05-20 12:21:30 +0300

    .

--- network.py
+++ network.py
@@ -5,37 +5,40 @@ from data_read import Read
 
 
 class Network():
-    def pandas_mat_2_stack(init,dataframe):
+    def pandas_mat_2_stack(init, dataframe):
         stack = dataframe.stack()
-        stack1 = stack[stack >= 1].rename_axis(('source', 'target')).reset_index(name='weight')
-        stack2 = stack[stack == 0].rename_axis(('source', 'target')).reset_index(name='weight')
-        all_stack = pd.concat([stack1,stack2])
+        stack1 = stack[stack >= 1].rename_axis(
+            ('source', 'target')).reset_index(name='weight')
+        stack2 = stack[stack == 0].rename_axis(
+            ('source', 'target')).reset_index(name='weight')
+        all_stack = pd.concat([stack1, stack2])
         return all_stack
 
-    def stack_2_graph(init,stack):
-        graph = nx.from_pandas_edgelist(stack,edge_attr=True)
+    def stack_2_graph(init, stack):
+        graph = nx.from_pandas_edgelist(
+            stack, source="source", target="target", edge_attr=True, create_using=nx.DiGraph)
         return graph
 
-    
-    def plot_graph(init,Graph,stack):
-        plt.figure(figsize = (100,70))
+    def plot_graph(init, Graph, stack):
+        plt.figure(figsize=(100, 70))
         nx.draw(Graph,
                 with_labels=True,
-                node_color = "red",
-                node_size = [v * 5 for v in dict(Graph.degree()).values()],
-                width = [v for v in stack["weight"].to_numpy()] 
+                node_color="red",
+                node_size=[v * 5 for v in dict(Graph.degree()).values()],
+                width=[v for v in stack["weight"].to_numpy()]
                 )
         ax = plt.gca()
         ax.collections[0].set_edgecolor("#687295")
         plt.show()
-        
+
+
 def main():
     net = Network()
     pr_pr = Read().mat_protein_protein()
-    stack =net.pandas_mat_2_stack(pr_pr)
+    stack = net.pandas_mat_2_stack(pr_pr)
     graph = net.stack_2_graph(stack)
-    net.plot_graph(graph,stack)
+    net.plot_graph(graph, stack)
 
 
 if __name__ == '__main__':
-    main()    
+    main()
---
+++ node_2_vec.py
@@ -0,0 +1,23 @@
+from node2vec import Node2Vec
+from gensim.test.utils import common_texts
+from gensim.models import Word2Vec
+
+class NodeVec:
+    def node_2_vec(init,graph,save_name):
+        node2vec = Node2Vec(graph,dimensions = 64,walk_length=800,num_walks = 10,weight_key="weight")
+        model = node2vec.fit(batch_words=32)  
+        model.save(save_name)
+
+    def returnSimilars(init,model,value):
+        return model.wv.most_similar(value)
+
+
+
+    def printElements(init,output,item):
+        elements = []
+        for a in x:
+            if a[0][0:2] == item:
+                elements.append(a)
+        return elements
+
+    
